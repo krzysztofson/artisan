4439e79cfb0b29f8d8e38b60188c23ba1282d45b
Krzysztof Dabrowski <krzysztofson@gmail.com>
2025-05-12 14:16:46 +0200

    template

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ["Poppins", "sans-serif"],
+        heading: ["Melodrama", "serif"],
+      },
+      colors: {
+        primary: "#3c6ca8",
+        secondary: "#8199bd",
+        accent: "#403c24",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
