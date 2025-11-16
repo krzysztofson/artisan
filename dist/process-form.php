e87679d0a565864bc48211d6f80ee245a448e2f0
Krzysztof Dabrowski <krzysztofson@gmail.com>
2025-11-16 16:29:24 +0100

    remove gtm form

--- dist/process-form.php
+++ dist/process-form.php
@@ -271,13 +271,6 @@ function createHtmlMessage($messageLines, $attachments) {
     $html = '<!DOCTYPE html>
 <html>
 <head>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
-new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
-\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,\'script\',\'dataLayer\',\'GTM-PFBHS9QL\');</script>
-<!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <style>
         body { font-family: Arial, sans-serif; line-height: 1.6; color: #333; }
